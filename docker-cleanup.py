c28797f06c9694c72702383ecbfafdefe9085ecf
wuxiaojun1027 <nhwuxiaojun@126.com>
2017-12-27 10:27:30 +0800

    add docker cleanup with python

--- docker-cleanup.py
+++ docker-cleanup.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*
 # remove old images and stopped containers
 
-import docker
+import os
 
-client = docker.DockerClient(version='1.24')  # macth the API version with commad ( docker version)
+# All images
+all_images_ret = os.popen("docker images --no-trunc | grep -v REPOSITORY ")
+all_images_list = all_images_ret.readlines()
 
-print(client)
 
-container_list = client.containers.list()
+ret = os.popen("docker ps -a |grep -v CREATED|awk \'{print $2}\'")
+used_image_list = ret.readlines()
+
+for item_of_list in all_images_list:
+    image_list = item_of_list.replace("\n", "").split()
+    image_name = image_list[0] + ":" + image_list[1]
+    image_id = image_list[2]
+
+    if str(used_image_list).find(image_name) == int('-1'):
+        print(image_name + " is not used")
+        exec_shell = "docker rmi " + image_id
+        del_ret = os.system(exec_shell)
+        print(del_ret)
+        if del_ret == 0:
+            print(image_name + " has been deleted!")
